e79b8756a4843a98689dc5e1662e27eeee3f278d
unknown <manishz90@yahoo.co.in>
2017-08-26 23:25:17 +0530

    Add delete /todos/:id route

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
 
 var app = express();
-
+// this will be when app is running on heroku and not locally.
+const port = process.env.PORT || 3000;
 
 
 app.use(bodyParser.json());
@@ -48,8 +49,25 @@ app.get('/todos/:id', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("Started on port 3000");
+app.delete("/todos/:id", (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }, () => {
+    res.status(400).send();
+  });
+});
+
+app.listen(port, () => {
+  console.log("Started on port " + port);
 });
 
 module.exports = {app};
